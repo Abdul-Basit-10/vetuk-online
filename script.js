fbfbf38f8c10711701f4e98a957f093079955a9e
hbl.com <hbl@gmail.com>
2023-09-26 23:16:01 +0500

    Final design changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const hamburgerMenuIcon = document.querySelector(".ham-icon");
 const hamburgerCloseIcon = document.querySelector(".close-icon");
 const nav_header = document.querySelector(".mobile-links");
+const nav = document.querySelector(".nav");
 
 function showMenu() {
   nav_header.classList.add("active");
@@ -15,4 +16,9 @@ hamburgerMenuIcon.addEventListener("click", () => {
 });
 hamburgerCloseIcon.addEventListener("click", () => {
   hideMenu();
+});
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 30) {
+    nav.classList.add("scrolled");
+  } else if (window.scrollY === 0) nav.classList.remove("scrolled");
 });
